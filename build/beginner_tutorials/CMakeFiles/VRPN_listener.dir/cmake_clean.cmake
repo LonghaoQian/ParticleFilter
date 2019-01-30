ed8090f0f7b42e5e07f8542ab48ff6fedc0889f8
longhao <longhao.qian@mail.utoronto.ca>
2019-01-30 17:51:44 -0500

    first_commit

---
+++ build/beginner_tutorials/CMakeFiles/VRPN_listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VRPN_listener.dir/src/VRPN_listener.cpp.o"
+  "/home/longhao/catkin_ws/devel/lib/beginner_tutorials/VRPN_listener.pdb"
+  "/home/longhao/catkin_ws/devel/lib/beginner_tutorials/VRPN_listener"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VRPN_listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
